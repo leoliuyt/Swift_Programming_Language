e106e1915b20ddfe22f9c84bb2ee373ac55c5843
leoliuyt <leoliuyt@sina.cn>
2016-12-03 16:58:43 +0800

    ARC

--- ARCDemo.playground/Contents.swift
+++ ARCDemo.playground/Contents.swift
@@ -2,4 +2,49 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    var apartment: Apartment?
+    deinit {
+        print("\(name) deinit")
+    }
+}
+
+//:Strong Reference Cycles Between Class Instances
+class Apartment {
+    let unit: String
+    init(unit: String) {
+        self.unit = unit
+    }
+    var tenant: Person?
+    deinit {
+        print("Apartment \(unit) is being deinitialized")
+    }
+}
+
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+
+reference1 = Person(name: "Tom")
+reference2 = reference1
+reference3 = reference1
+
+reference1 = nil
+reference2 = nil
+reference3 = nil
+
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "John")
+unit4A = Apartment(unit: "4A")
+
+john?.apartment = unit4A
+unit4A?.tenant = john
+
+john = nil
+unit4A = nil
